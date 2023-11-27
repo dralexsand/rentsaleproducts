93ed4576f22f2bf16d2329302d4dec24122bec2b
dralexsand <dr.alex.sand.111@gmail.com>
2023-11-28 02:56:43 +0600

    add auth

--- apps/backend/php/routes/api/v1/api.php
+++ apps/backend/php/routes/api/v1/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::name('api.')->group(function () {
+    Route::name('auth.')->prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register'])->name('register');
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+
+        Route::group(['middleware' => ['auth:api']], function () {
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+    });
 });
